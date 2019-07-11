7ddcaeb7d1f1160e9e9546e531979764753ab3e0
Kchriss <52273624+Kchriss@users.noreply.github.com>
2019-07-11 07:44:27 +0200

    Add files via upload

---
+++ Parcequejaienvidecommenceralancienneavecunsimplefichierjs.js
@@ -0,0 +1,3 @@
+//https://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/1922300-lapi-file
+//http://www.script-tutorials.com/html5-drag-and-drop-multiple-file-uploader/
+//http://www.maximechaillou.com/simple-upload-en-drag-and-drop-avec-html5-jquery-php/
