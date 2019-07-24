a11014c7bdc14457720ac6da7e551a9b0307442c
Kchriss <52273624+Kchriss@users.noreply.github.com>
2019-07-24 21:20:32 +0200

    new branch 24/072019

--- GameParseEQ.js
+++ GameParseEQ.js
@@ -1,11 +1,10 @@
-var CurrentPlayer;
-var HealType = "";
-var FilePath;
-var files;
-var tabFile;
-var logHeal=[];
-var timeFilter=[];
-var dropper = document.querySelector('#dropper');
+let CurrentPlayer;
+let HealType = "";
+let files;
+let tabFile;
+let logHeal=[];
+let timeFilter=[];
+let dropper = document.querySelector('#dropper');
 
 //drop zone for file event dragover and drop needed to get file data
 dropper.addEventListener('dragover', function (e) {
@@ -30,7 +29,7 @@ dropper.addEventListener('drop', function (e) {
 
 
         // "show must go on " a to do .... split data form each line .... get name player list( check for another way) //  postion after date log
-        for (var i = 0; i < tabFile.length; i++) {
+        for (let i = 0; i < tabFile.length; i++) {
             readerdigest(tabFile, i);
             // note faire une recherche sur healed => pour determiner sa presence et ca position ...
             // pour faire les substring et le check de la ligne renvoie des donnees extraite dans le tableau
@@ -48,10 +47,10 @@ dropper.addEventListener('drop', function (e) {
 
 
 }, false);
-var k=0;
+let k=0;
 
 function readerdigest(tabFile, i) {
-    let curserTag = tabFile[i].match(/backstabs|begins|crushes|frenzies|healed|hits|pierces|shoots|slashes|taken/gi);//test le mot cle et recupere sa valeur ! :p
+    let curserTag = tabFile[i].match(/backstabs|begins|crushes|frenzies|healed|hits|pierces|shoots|slashes|taken/gi); //test le mot cle et recupere sa valeur ! :p
     let curser = tabFile[i].indexOf(curserTag, 0);
     if (curserTag !== null) {// sorting by key word with indexof ....
         switch (curserTag.toString().trim()) {
@@ -113,7 +112,7 @@ class Taken {
         curser_2 = this.tabFile[this.i].lastIndexOf(' ', curser_1 - 1);
         curser_3 = this.tabFile[this.i].lastIndexOf(' ', curser_2 - 1);
         this.playerDmged = this.tabFile[this.i].substr(curser_3, curser_2 - curser_3);//extraction du nom du joueur
-        if (this.playerDmged.trim() == "you") {
+        if (this.playerDmged.trim() === "you") {
             this.playerDmged = CurrentPlayer;
         }
         return this.playerDmged;
@@ -212,7 +211,7 @@ class Healed {
         this.curser_1 = this.tabFile[this.i].lastIndexOf(' ', this.curser);
         this.curser_2 = this.tabFile[this.i].lastIndexOf(' ', this.curser_1 - 1);
         this.playerHealer = this.tabFile[this.i].substr(this.curser_2, this.curser_1 - this.curser_2).trim();//extraction du nom du joueur
-        if (this.playerHealer.trim().toLowerCase() == "you") {
+        if (this.playerHealer.trim().toLowerCase() === "you") {
             this.playerHealer = CurrentPlayer;
         }
         return this.playerHealer;
@@ -222,10 +221,10 @@ class Healed {
         this.curser_3 = this.tabFile[this.i].indexOf(' ', this.curser + 1);
         this.curser_4 = this.tabFile[this.i].indexOf(' ', this.curser_3 + 1);
         this.playerHealed = this.tabFile[this.i].substr(this.curser_3, this.curser_4 - this.curser_3).trim();//extraction du nom du joueur
-        if (this.playerHealed.trim() == "himself" || this.playerHealed.trim() == "itself" || this.playerHealed.trim() == "herself") {
+        if (this.playerHealed.trim() === "himself" || this.playerHealed.trim() === "itself" || this.playerHealed.trim() === "herself") {
             this.playerHealed = this.playerHealer;
         }
-        else if (this.playerHealed.trim().toLowerCase() == "you") {
+        else if (this.playerHealed.trim().toLowerCase() === "you") {
             this.playerHealed = CurrentPlayer;
         }
         return this.playerHealed;
@@ -252,7 +251,7 @@ class Healed {
     OverHeal() {
         this.curser_8 = this.tabFile[this.i].indexOf(' ', this.curser_7 + 1);
         let OverHealAmount = this.tabFile[this.i].substr(this.curser_7, 2).trim();//extraction du nom du joueur
-        if (OverHealAmount !== null && OverHealAmount == "(") {
+        if (OverHealAmount !== null && OverHealAmount === "(") {
             OverHealAmount = this.tabFile[this.i].substr(this.curser_7+2, this.curser_8-3 - this.curser_7).trim()
         } else {
             OverHealAmount = 0;
@@ -267,8 +266,8 @@ class Healed {
         if (this.curser_9 > 0 && this.curser_9 !== null) {
             this.curser_10 = this.tabFile[this.i].indexOf(' ', this.curser_9);
             this.curser_11 = this.tabFile[this.i].indexOf('.', this.curser_10);
-            let spellrankchecking = this.tabFile[this.i].substr(this.curser_10, this.curser_11 - this.curser_10).match(/rk/gi);
-            if (spellrankchecking !== null && spellrankchecking == "Rk") {
+            let spellRkChecking = this.tabFile[this.i].substr(this.curser_10, this.curser_11 - this.curser_10).match(/rk/gi);
+            if (spellRkChecking !== null && spellRkChecking == "Rk") {
                 this.curser_12 = this.tabFile[this.i].indexOf('.', this.curser_11 + 1);
                 SpellsCasted = this.tabFile[this.i].substr(this.curser_10, this.curser_12 - this.curser_10).trim();
             } else {
@@ -282,18 +281,18 @@ class Healed {
     }
 
     CriticalHitMessage() {
-        let Critmess = "";
+        let messCrtHeal = "";
         if (this.curser_10 !== null) {
             this.curser_13 = this.tabFile[this.i].indexOf('(', this.curser_10);
 
             if (this.curser_13 !== null && this.curser_13 > 0) {
                 this.curser_14 = this.tabFile[this.i].indexOf(')', this.curser_13);
-                Critmess = this.tabFile[this.i].substr(this.curser_13, this.curser_14 + 1 - this.curser_13).trim();
+                messCrtHeal = this.tabFile[this.i].substr(this.curser_13, this.curser_14 + 1 - this.curser_13).trim();
             } else {
-                Critmess = ""
+                messCrtHeal = ""
             }
         }
-        return Critmess;
+        return messCrtHeal;
     }
 
     Logtime() {
@@ -306,17 +305,17 @@ class Healed {
 
 let input =document.getElementById('radioInputChoice');
 input.step=0.1;
-let choice1 = document.getElementById('Choice1')
+let choice1 = document.getElementById('Choice1');
 choice1.addEventListener('click', updateBtn);
-var choice2 = document.getElementById('Choice2');
+let choice2 = document.getElementById('Choice2');
 choice2.addEventListener('click', updateBtn);
-var choice3 = document.getElementById('Choice3');
+let choice3 = document.getElementById('Choice3');
 choice3.addEventListener('click', updateBtn);
-var choice4 = document.getElementById('Choice4');
+let choice4 = document.getElementById('Choice4');
 choice4.addEventListener('click', updateBtn);
-var choice5 = document.getElementById('Choice5');
+let choice5 = document.getElementById('Choice5');
 choice5.addEventListener('click', updateBtn);
-var choice6 = document.getElementById('Choice6');
+let choice6 = document.getElementById('Choice6');
 choice6.addEventListener('click', updateBtn);
 
 function updateBtn() {
@@ -368,7 +367,7 @@ function menudropplayers(players,playersList) {
     dropdown.add(defaultOption);
     dropdown.style.visibility = "visible";
     dropdown.innerText="";
-    dropdown.addEventListener("change", addActivityItem, false)
+    dropdown.addEventListener("change", addActivityItem, false);
     dropdown.selectedIndex = 0;
     for (let i = 0; i < playersList.length; i++) {
      let option = document.createElement('option');
@@ -444,7 +443,7 @@ function containerPlayer(targedhealer,playerTargetByHealer,total,total2,counter)
     }
 }
 function openCity(evt, Name) {
-    var i, tabcontent, tablinks;
+    let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -461,39 +460,7 @@ function openCity(evt, Name) {
 //drop zone for file event dragover and drop needed to get file data
 /*
 
-var tablinks = document.querySelector('#tablinks');
 
-tablinks.addEventListener('click', function (e) {
-    e.preventDefault();
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById('container'+ this.elements.id).style.display = "block";
-    e.currentTarget.className += " active";
-}, false);
-/*
-var tablinks = document.querySelector('#tablinks');
-
-function openCity(evt, Name) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById('container'+ this.elements.id).style.display = "block";
-    e.currentTarget.className += " active";
-}
-tablinks.addEventListener('click',openCity(evt,Name));
 
 
  !!!!!!! Tabs,pills, dropdown !!!!!
@@ -505,7 +472,7 @@ tablinks.addEventListener('click',openCity(evt,Name));
  !!!!!!! stream et map a voir !!!!!
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- http://cryto.net/~joepie91/blog/2015/05/04/functional-programming-in-javascript-map-filter-reduce/
+
  logHeal[i]={"id":i,"healer":logHealed.PlayerHealer(),"healed":logHealed.TargedHealed()
                                     ,"type":logHealed.TypeOfHeal(),"Heal":logHealed.HealAmount(),"overheal":logHealed.OverHeal()
                                     ,'spell':logHealed.SpellsUsed(),"crit":logHealed.CriticalHitMessage(),"logDate":logHealed.Logtime()};
@@ -520,24 +487,4 @@ tablinks.addEventListener('click',openCity(evt,Name));
 [Mon Jul 08 22:21:03 2019] Katercat healed Anlak for 48008 (63059) hit points by Spiritual Squall Rk. III. (Critical)
 [Mon Jul 08 22:21:03 2019] Venedar healed Folkken over time for 1128 hit points by Prophet's Gift of the Ruchu. (Lucky Critical)
 
-https://developer.mozilla.org/fr/docs/Learn/JavaScript/Objects/JSON
-https://openclassrooms.com/forum/sujet/lire-un-fichier-texte-en-javascript-33614
-https://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/1922300-lapi-file
-http://www.script-tutorials.com/html5-drag-and-drop-multiple-file-uploader/
-https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
-https://stackoverflow.com/questions/40774697/how-to-group-an-array-of-objects-by-key/40774906
-https://www.freecodecamp.org/news/15-useful-javascript-examples-of-map-reduce-and-filter-74cbbb5e0a1f/
-https://codeburst.io/grouping-array-data-json-ef96b438b927
-https://gist.github.com/JamieMason/0566f8412af9fe6a1d470aa1e089a752
-http://www.supportduweb.com/scripts_tutoriaux-code-source-48-systeme-d-039-onglets-en-javascript-x-html-et-css-dans-la-meme-page.html
-http://www.maximechaillou.com/simple-upload-en-drag-and-drop-avec-html5-jquery-php/
-ttps://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/1922300-lapi-file
-
- extraire les données et les placer dans un tableau ... nom (ou you) heal ... overheal ... nom player healer '(ou myself... himself...) etc....
-
-https://stackoverflow.com/questions/11199653/javascript-sum-and-group-by-of-json-data
-http://learnjsdata.com/group_data.html
-
-https://stackoverflow.com/questions/5041270/what-is-the-best-way-to-use-context-param-in-javascript
-https://developer.mozilla.org/fr/docs/Web/JavaScript/Les_diff%C3%A9rents_tests_d_%C3%A9galit%C3%A9
 */
--- README.md
+++ README.md
@@ -11,3 +11,28 @@ des explications sur le doc?
 une aider a l utilisateur ??
 un café et l addition ?
 
+
+
+source
+ http://cryto.net/~joepie91/blog/2015/05/04/functional-programming-in-javascript-map-filter-reduce/
+https://developer.mozilla.org/fr/docs/Learn/JavaScript/Objects/JSON
+https://openclassrooms.com/forum/sujet/lire-un-fichier-texte-en-javascript-33614
+https://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/1922300-lapi-file
+http://www.script-tutorials.com/html5-drag-and-drop-multiple-file-uploader/
+https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
+https://stackoverflow.com/questions/40774697/how-to-group-an-array-of-objects-by-key/40774906
+https://www.freecodecamp.org/news/15-useful-javascript-examples-of-map-reduce-and-filter-74cbbb5e0a1f/
+https://codeburst.io/grouping-array-data-json-ef96b438b927
+https://gist.github.com/JamieMason/0566f8412af9fe6a1d470aa1e089a752
+http://www.supportduweb.com/scripts_tutoriaux-code-source-48-systeme-d-039-onglets-en-javascript-x-html-et-css-dans-la-meme-page.html
+http://www.maximechaillou.com/simple-upload-en-drag-and-drop-avec-html5-jquery-php/
+ttps://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/1922300-lapi-file
+
+ extraire les données et les placer dans un tableau ... nom (ou you) heal ... overheal ... nom player healer '(ou myself... himself...) etc....
+
+https://stackoverflow.com/questions/11199653/javascript-sum-and-group-by-of-json-data
+http://learnjsdata.com/group_data.html
+
+https://stackoverflow.com/questions/5041270/what-is-the-best-way-to-use-context-param-in-javascript
+https://developer.mozilla.org/fr/docs/Web/JavaScript/Les_diff%C3%A9rents_tests_d_%C3%A9galit%C3%A9
+
